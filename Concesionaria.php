ba64da23298fe1300cb2e9a979446f882bc29cbf
thadek <gabipamic@gmail.com>
2023-06-01 15:05:46 -0300

    cambios ventas

--- Concesionaria.php
+++ Concesionaria.php
@@ -2,15 +2,17 @@
 
 require_once("Venta.php");
 
-class Concesionaria {
+class Concesionaria
+{
 
     private $denominacion;
     private $direccion;
-    private $clientes; 
-    private $vehiculos; 
-    private $ventasRealizadas; 
+    private $clientes;
+    private $vehiculos;
+    private $ventasRealizadas;
 
-    public function __construct($denominacion, $direccion, $clientes, $vehiculos, $ventasRealizadas){
+    public function __construct($denominacion, $direccion, $clientes, $vehiculos, $ventasRealizadas)
+    {
         $this->denominacion = $denominacion;
         $this->direccion = $direccion;
         $this->clientes = $clientes;
@@ -20,43 +22,53 @@ class Concesionaria {
 
 
     //Metodos de acceso.
-    public function getDenominacion(){
+    public function getDenominacion()
+    {
         return $this->denominacion;
     }
 
-    public function getDireccion(){
+    public function getDireccion()
+    {
         return $this->direccion;
     }
 
-    public function getClientes(){
+    public function getClientes()
+    {
         return $this->clientes;
     }
 
-    public function getVehiculos(){
+    public function getVehiculos()
+    {
         return $this->vehiculos;
     }
 
-    public function getVentasRealizadas(){
+    public function getVentasRealizadas()
+    {
         return $this->ventasRealizadas;
     }
 
-    public function setDenominacion($denominacion){
+    public function setDenominacion($denominacion)
+    {
         $this->denominacion = $denominacion;
     }
 
-    public function setDireccion($direccion){
+    public function setDireccion($direccion)
+    {
         $this->direccion = $direccion;
     }
 
-    public function setClientes($clientes){
+    public function setClientes($clientes)
+    {
         $this->clientes = $clientes;
     }
 
-    public function setVehiculos($vehiculos){
+    public function setVehiculos($vehiculos)
+    {
         $this->vehiculos = $vehiculos;
     }
 
-    public function setVentasRealizadas($ventasRealizadas){
+    public function setVentasRealizadas($ventasRealizadas)
+    {
         $this->ventasRealizadas = $ventasRealizadas;
     }
 
@@ -64,29 +76,32 @@ class Concesionaria {
     /**
      * Retorna un string mostrando los clientes que contiene el arreglo de clientes de la concesionaria.
      */
-    public function mostrarClientes(){
+    public function mostrarClientes()
+    {
         $respuesta = "";
         $clientes = $this->getClientes();
-        for($i=0;$i<count($clientes);$i++){
-            $respuesta.= $clientes[$i]. "\n";
+        for ($i = 0; $i < count($clientes); $i++) {
+            $respuesta .= $clientes[$i] . "\n";
         }
         return $respuesta;
     }
 
-    public function mostrarVehiculos(){
+    public function mostrarVehiculos()
+    {
         $respuesta = "";
         $vehiculos = $this->getVehiculos();
-        for($i=0;$i<count($vehiculos);$i++){
-            $respuesta.= $vehiculos[$i] . "\n";
+        for ($i = 0; $i < count($vehiculos); $i++) {
+            $respuesta .= $vehiculos[$i] . "\n";
         }
         return $respuesta;
     }
 
-    public function mostrarVentasRealizadas(){
+    public function mostrarVentasRealizadas()
+    {
         $respuesta = "";
         $ventas = $this->getVentasRealizadas();
-        for($i=0;$i<count($ventas);$i++){
-            $respuesta.= $ventas[$i] . "\n";
+        for ($i = 0; $i < count($ventas); $i++) {
+            $respuesta .= $ventas[$i] . "\n";
         }
         return $respuesta;
     }
@@ -94,14 +109,14 @@ class Concesionaria {
 
 
     //toString
-    public function __toString(){
+    public function __toString()
+    {
         return "Concesionaria: " .
-        "\nDenominación: " . $this->getDenominacion() .
-        "\nDireccion: " . $this->getDireccion() .
-        "\nClientes: "  . $this->mostrarClientes() .
-        "\nVehiculos: " . $this->mostrarVehiculos() .
-        "\nVentas Realizadas: " . $this->mostrarVentasRealizadas();
-
+            "\nDenominación: " . $this->getDenominacion() .
+            "\nDireccion: " . $this->getDireccion() .
+            "\nClientes: "  . $this->mostrarClientes() .
+            "\nVehiculos: " . $this->mostrarVehiculos() .
+            "\nVentas Realizadas: " . $this->mostrarVentasRealizadas();
     }
 
     /**
@@ -110,14 +125,15 @@ class Concesionaria {
      * @param int $codigoV
      * @return Vehiculo
      */
-    public function retornarVehiculo($codigoV){
+    public function retornarVehiculo($codigoV)
+    {
         //Recorrido parcial del arreglo de vehiculos.-
         $i = 0;
         $corte = false;
         $retorno = null;
         $vehiculos = $this->getVehiculos();
-        while($i<count($vehiculos) && !$corte){
-            if($vehiculos[$i]->getCodigo() === $codigoV){
+        while ($i < count($vehiculos) && !$corte) {
+            if ($vehiculos[$i]->getCodigo() === $codigoV) {
                 $retorno = $vehiculos[$i];
                 $corte = true;
             }
@@ -129,38 +145,47 @@ class Concesionaria {
 
     /**
      * Esta funcion recibe una coleccion de codigos de vehiculos y un objeto cliente.
+     * Verifica que el cliente este activo, si es asi crea una venta con la fecha actual y el cliente recibido.
+     * Recorre la coleccion de codigos y agrega a la venta los vehiculos que coincidan con el codigo.
+     * Si la venta tiene al menos un vehiculo, la agrega al arreglo de ventas de la concesionaria.
+     * Retorna el precio final de la venta.
+     * @param array $colCodigosVehiculos
+     * @param Cliente $objCliente
+     * @return int
      */
-    public function registrarVenta($colCodigosVehiculos,$objCliente){
-        
-        //Verifico que el cliente este activo
-        $vehiculosAVender= array();
-        $retorno = 0;
-        
-        if($objCliente->getIsActivo()){
-            
-            $venta = new Venta(date("d/m/Y"),$objCliente,$vehiculosAVender);
+    public function registrarVenta($colCodigosVehiculos, $objCliente)
+    {
 
-        //Recorro la coleccion de codigos y agrego a la venta los vehiculos que coincidan con el codigo.
-            for($i=0;$i<count($colCodigosVehiculos);$i++){
+        //Creo un arreglo vacio de vehiculos a vender.
+        $vehiculosAVender = array();
+        $retorno = 0;
+
+        //Verifico que el cliente este activo
+        if ($objCliente->getIsActivo()) {
+
+            $venta = new Venta(date("d/m/Y"), $objCliente, $vehiculosAVender);
+
+            //Recorro la coleccion de codigos y agrego a la venta los vehiculos que coincidan con el codigo.
+            for ($i = 0; $i < count($colCodigosVehiculos); $i++) {
                 $vehiculo = $this->retornarVehiculo($colCodigosVehiculos[$i]);
-                if($vehiculo != null){
+                if ($vehiculo != null) {
                     $venta->incorporarVehiculo($vehiculo);
                 }
-
             }
-
-            //Agrego la venta al arreglo de ventas de la concesionaria.
-            $ventas = $this->getVentasRealizadas();
-            array_push($ventas,$venta);
-            $this->setVentasRealizadas($ventas);
-
-            $retorno = $venta->getPrecioFinal();
+            //Verifico que la venta tenga al menos un vehiculo. Si es asi, la agrego al arreglo de ventas de la concesionaria.
+            if (count($venta->getVehiculos()) > 0) {
+                //Agrego la venta al arreglo de ventas de la concesionaria.
+                $ventas = $this->getVentasRealizadas();
+                array_push($ventas, $venta);
+                $this->setVentasRealizadas($ventas);
+                $retorno = $venta->getPrecioFinal();
+            //Si no tiene vehiculos, seteo la venta en null.
+            }else{
+                $venta = null;
+            }
         }
 
         return $retorno;
-        
-
-
     }
 
 
@@ -170,19 +195,16 @@ class Concesionaria {
      * @param int $nroDoc
      * @return array
      */
-    public function retornarVentasXCliente($tipo,$nroDoc){
+    public function retornarVentasXCliente($tipo, $nroDoc)
+    {
         $arrVentas = $this->getVentasRealizadas();
         $arrVentasCliente = array();
-        for($i=0;$i<count($arrVentas);$i++){
-            if($arrVentas[$i]->getCliente()->getTipoDni() === $tipo && $arrVentas[$i]->getCliente()->getDni() === $nroDoc){
-                array_push($arrVentasCliente,$arrVentas[$i]);
+        for ($i = 0; $i < count($arrVentas); $i++) {
+            if ($arrVentas[$i]->getCliente()->getTipoDni() === $tipo && $arrVentas[$i]->getCliente()->getDni() === $nroDoc) {
+                array_push($arrVentasCliente, $arrVentas[$i]);
             }
         }
-        
+
         return $arrVentasCliente;
     }
-
-
-
-
 }
